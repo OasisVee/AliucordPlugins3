79782941f0b4b3ae44b351a16998c2988e0521eb
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-19 17:22:54 -0400

    Update Settings.kt

--- UITH/src/main/java/com/scruzism/plugins/Settings.kt
+++ UITH/src/main/java/com/scruzism/plugins/Settings.kt
@@ -1,14 +1,11 @@
 package com.scruzism.plugins
 
 import android.view.View
-import android.text.Editable
 import android.widget.TextView
 import android.annotation.SuppressLint
 import android.text.util.Linkify
 
 import com.aliucord.Utils
-import com.aliucord.views.Button
-import com.aliucord.views.TextInput
 import com.aliucord.api.SettingsAPI
 import com.aliucord.fragments.SettingsPage
 import com.aliucord.utils.DimenUtils
@@ -16,7 +13,6 @@ import com.aliucord.views.Divider
 
 import com.lytefast.flexinput.R
 import com.discord.utilities.color.ColorCompat
-import com.discord.utilities.view.text.TextWatcher
 import com.discord.views.CheckedSetting
 
 
@@ -25,54 +21,29 @@ class PluginSettings(private val settings: SettingsAPI) : SettingsPage() {
     @SuppressLint("SetTextI18n")
     override fun onViewBound(view: View?) {
         super.onViewBound(view)
-        setActionBarTitle("UITH")
+        setActionBarTitle("UITH - Catbox.moe")
         val ctx = requireContext()
         val p = DimenUtils.defaultPadding
 
-        val errorField = TextView(ctx).apply {
-            setTextColor(ColorCompat.getThemedColor(ctx, R.b.colorError))
-            setPadding(p, p, p, p)
-        }
-
         // HEADER
         TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply {
-            text = "Regex Settings"
+            text = "Catbox.moe Image Uploader"
             addView(this)
         }
 
         // TEXT
         TextView(ctx).apply {
-            text = "Enter the regex pattern of the URL to receive"
+            text = "This plugin uploads images to catbox.moe instead of Discord's servers."
             setTextColor(ColorCompat.getThemedColor(ctx, R.b.colorOnPrimary))
             setPadding(p, p, p, p)
             addView(this)
-        }
-
-        // INPUT BOX
-        val input = TextInput(ctx, "Regex")
-        input.apply {
-            editText.setText(settings.getString("regex", "https:\\/\\/[\\w./-]*"))
-            editText.addTextChangedListener(object : TextWatcher() {
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-                override fun afterTextChanged(s: Editable) {}
-            })
-        }
-
-        // SAVE BUTTON
-        val button = Button(ctx).apply {
-            text = "Save"
-            setOnClickListener {
-                settings.setString("regex", input.editText.text.toString().toRegex().toString())
-                Utils.showToast("Saved")
-            }
         }
 
         // DIV
         val divider = Divider(ctx).apply { setPadding(p, p, p, p) }
 
         // ADVANCED SETTINGS HEADER
-        val advHeader = TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply { text = "Advanced Settings" }
+        val advHeader = TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply { text = "Settings" }
 
         // CHECKED SETTINGS
         val uploadAllAttachments = Utils.createCheckedSetting(
@@ -97,29 +68,25 @@ class PluginSettings(private val settings: SettingsAPI) : SettingsPage() {
         // 2nd DIV
         val secondDivider = Divider(ctx).apply { setPadding(p, p, p, p) }
 
-        // LINKS HEADER
-        val linksHeader = TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply { text = "Links" }
+        // ABOUT HEADER
+        val aboutHeader = TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply { text = "About" }
 
-        // HELP/INFO
-        val helpInfo = TextView(ctx).apply {
-            linksClickable = true
-            text = "- Support Server: https://discord.gg/tdjBfsvhHT\n- UITH README: https://git.io/JSyri"
+        // ABOUT TEXT 
+        val aboutText = TextView(ctx).apply {
+            text = "This plugin uploads images to catbox.moe instead of Discord's servers. Catbox.moe is a free image hosting service with no account required.\n\n" +
+                   "When you send an image, it will be uploaded to catbox.moe and the link will be posted in the chat automatically."
             setTextColor(ColorCompat.getThemedColor(ctx, R.b.colorOnPrimary))
+            setPadding(p, p, p, p)
         }
-        Linkify.addLinks(helpInfo, Linkify.WEB_URLS)
-
-        addView(input)
-        addView(button)
-        addView(errorField)
 
         addView(divider)
-
+        
         addView(advHeader)
         addView(uploadAllAttachments)
         addView(switchOffPlugin)
-
+        
         addView(secondDivider)
-        addView(linksHeader)
-        addView(helpInfo)
+        addView(aboutHeader)
+        addView(aboutText)
     }
 }
