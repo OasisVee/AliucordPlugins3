61bbde6b5469ce005630a63b69ddc042cbbc965d
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-23 19:42:55 -0400

    Update ScreenshotAPI.kt

--- ScreenshotAPI/src/main/java/com/scruzism/plugins/ScreenshotAPI.kt
+++ ScreenshotAPI/src/main/java/com/scruzism/plugins/ScreenshotAPI.kt
@@ -9,15 +9,17 @@ import com.aliucord.api.CommandsAPI
 import com.aliucord.entities.Plugin
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.MessageEmbedBuilder
+import com.aliucord.entities.PluginSettings
 import com.discord.api.commands.ApplicationCommandType
 import com.discord.api.message.embed.MessageEmbed
+import com.discord.views.CheckedSetting
+import com.lytefast.flexinput.R
 
 import java.net.URLEncoder
 
 data class APIResponse(
-	val screenshot: String
+    val screenshot: String
 )
-
 
 @AliucordPlugin
 class ScreenshotAPI : Plugin() {
@@ -43,23 +45,25 @@ class ScreenshotAPI : Plugin() {
             val url = URLEncoder.encode(it.getRequiredString("url"))
             val shouldSend = it.getBoolOrDefault("send", false)
             try {
-                val httpUrl = StringBuilder("https://shot.screenshotapi.net/screenshot?token=8YB357P-BNR4620-M3JRHZ7-7DJRFJJ")
-				.append("&url=$url&output=json&file_type=png&block_ads=true&wait_for_event=load&ttl=15")
-				.toString()
-				log.debug(url)
-				val result = Http.Request(httpUrl, "GET").execute().json(APIResponse::class.java).screenshot
+                val apiKey = settings.getString("api_key", null)
+                if (apiKey.isNullOrEmpty()) {
+                    return@registerCommand CommandsAPI.CommandResult("API key is not set. Please configure the API key in settings.", null, false)
+                }
 
-				if (!shouldSend)
-				{
-					val embed = MessageEmbedBuilder().setRandomColor().setImage(result, null, 876, 1680).build()
-					return@registerCommand CommandsAPI.CommandResult(null, mutableListOf(embed), false, "ScreenshotAPI")
-				}
+                val httpUrl = StringBuilder("https://api.screenshotmachine.com?key=$apiKey&url=$url&dimension=1024x768&format=png")
+                    .toString()
+                log.debug(url)
+                val result = Http.Request(httpUrl, "GET").execute().json(APIResponse::class.java).screenshot
+
+                if (!shouldSend) {
+                    val embed = MessageEmbedBuilder().setRandomColor().setImage(result, null, 876, 1680).build()
+                    return@registerCommand CommandsAPI.CommandResult(null, mutableListOf(embed), false, "ScreenshotAPI")
+                }
 
                 CommandsAPI.CommandResult(result, null, shouldSend, "ScreenshotAPI")
-            }
-            catch (t: Throwable) {
+            } catch (t: Throwable) {
                 log.error(t)
-                CommandsAPI.CommandResult("An error occured. Check Debug Logs", null, false)
+                CommandsAPI.CommandResult("An error occurred. Check Debug Logs", null, false)
             }
         }
 
@@ -67,4 +71,13 @@ class ScreenshotAPI : Plugin() {
 
     override fun stop(ctx: Context) = commands.unregisterAll()
 
+    override fun getSettingsLayout(ctx: Context): PluginSettings {
+        val settings = PluginSettings()
+        val apiKeySetting = Utils.createCheckedSetting(ctx, CheckedSetting.ViewType.EDIT, "API Key", "Enter your API key here")
+        apiKeySetting.setOnCheckedListener { checked ->
+            settings.setString("api_key", apiKeySetting.editText.text.toString())
+        }
+        settings.addSetting(apiKeySetting)
+        return settings
+    }
 }
