9381ab7e5161e46b61f247a8137f5babb0f1eaef
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-27 01:36:34 -0400

    Update build.gradle.kts

--- UITH/build.gradle.kts
+++ UITH/build.gradle.kts
@@ -1,12 +1,17 @@
-version = "1.1.0"
+version = "1.1.1"
 description = "Upload images to catbox.moe directly from Discord."
 
 aliucord.changelog.set(
         """
-        1.1.0
+        # 1.1.1
+        * made catbox json default
+        * added a userhash option in settings for catbox
+        
+        # 1.1.0
         *  made catbox the default regex
         *  added gif and mp4 as part of default
-        1.0.0
+        
+        # 1.0.0
         * Initial release of catbox.moe version
         * Simplified plugin that uploads directly to catbox.moe
         * No sxcu configuration needed
