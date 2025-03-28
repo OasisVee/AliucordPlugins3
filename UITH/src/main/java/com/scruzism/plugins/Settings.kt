603d15c02c93bb4612130b651a7829e72f061a53
veeaces96@gmail.com <131408227+OasisVee@users.noreply.github.com>
2025-03-28 16:29:21 -0400

    made more presentable and made it dracula

--- UITH/src/main/java/com/scruzism/plugins/Settings.kt
+++ UITH/src/main/java/com/scruzism/plugins/Settings.kt
@@ -5,6 +5,10 @@ import android.text.Editable
 import android.widget.TextView
 import android.annotation.SuppressLint
 import android.text.util.Linkify
+import android.graphics.Typeface
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
 
 import com.aliucord.Utils
 import com.aliucord.views.Button
@@ -21,6 +25,20 @@ import com.discord.views.CheckedSetting
 
 class PluginSettings(private val settings: SettingsAPI) : SettingsPage() {
 
+    // Dracula Color Scheme
+    private val colors = object {
+        val background = 0xFF282A36.toInt()
+        val foreground = 0xFFF8F8F2.toInt()
+        val comment = 0xFF6272A4.toInt()
+        val cyan = 0xFF8BE9FD.toInt()
+        val green = 0xFF50FA7B.toInt()
+        val orange = 0xFFFFB86C.toInt()
+        val pink = 0xFFFF79C6.toInt()
+        val purple = 0xFFBD93F9.toInt()
+        val red = 0xFFFF5555.toInt()
+        val yellow = 0xFFF1FA8C.toInt()
+    }
+
     @SuppressLint("SetTextI18n")
     override fun onViewBound(view: View?) {
         super.onViewBound(view)
@@ -28,149 +46,150 @@ class PluginSettings(private val settings: SettingsAPI) : SettingsPage() {
         val ctx = requireContext()
         val p = DimenUtils.defaultPadding
 
-        val errorField = TextView(ctx).apply {
-            setTextColor(ColorCompat.getThemedColor(ctx, R.b.colorError))
+        // Main layout with Dracula background
+        view?.setBackgroundColor(colors.background)
+
+        // Create a vertical layout for better organization
+        val mainLayout = LinearLayout(ctx).apply {
+            orientation = LinearLayout.VERTICAL
+            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
             setPadding(p, p, p, p)
         }
 
-        // HEADER
-        TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply {
-            text = "Regex Settings"
-            addView(this)
+        // Section Header
+        fun createSectionHeader(text: String): TextView = TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply {
+            this.text = text
+            setTextColor(colors.purple)
+            typeface = Typeface.DEFAULT_BOLD
+            setPadding(0, p * 2, 0, p)
         }
 
-        // TEXT
-        TextView(ctx).apply {
-            text = "Enter the regex pattern of the URL to receive"
-            setTextColor(ColorCompat.getThemedColor(ctx, R.b.colorOnPrimary))
+        // Styled Text
+        fun createDescriptionText(text: String): TextView = TextView(ctx).apply {
+            this.text = text
+            setTextColor(colors.foreground)
             setPadding(p, p, p, p)
-            addView(this)
         }
 
-        // INPUT BOX
-        val input = TextInput(ctx, "Regex")
-        input.apply {
+        // Regex Settings Section
+        mainLayout.addView(createSectionHeader("Regex Settings"))
+        mainLayout.addView(createDescriptionText("Enter the regex pattern of the URL to receive"))
+
+        // Regex Input
+        val input = TextInput(ctx, "Regex").apply {
             editText.setText(settings.getString("regex", "https:\\/\\/files\\.catbox\\.moe\\/[\\w.-]*"))
-            editText.addTextChangedListener(object : TextWatcher() {
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-                override fun afterTextChanged(s: Editable) {}
-            })
+            editText.setTextColor(colors.foreground)
+            editText.setHintTextColor(colors.comment)
         }
+        mainLayout.addView(input)
 
-        // SAVE BUTTON
+        // Save Button
         val button = Button(ctx).apply {
             text = "Save"
+            setTextColor(colors.background)
+            setBackgroundColor(colors.green)
             setOnClickListener {
                 settings.setString("regex", input.editText.text.toString().toRegex().toString())
                 Utils.showToast("Saved")
             }
         }
+        mainLayout.addView(button)
 
-        // RESET JSON BUTTON
-        val resetButton = Button(ctx).apply {
-            text = "Reset JSON to Default"
-            setTextColor(ColorCompat.getThemedColor(ctx, R.b.colorOnError))
-            setBackgroundColor(ColorCompat.getThemedColor(ctx, R.b.colorError))
-            setOnClickListener {
-                settings.remove("jsonConfig")
-                Utils.showToast("JSON configuration reset to default")
-            }
-        }
+        // Divider
+        mainLayout.addView(Divider(ctx).apply { 
+            setBackgroundColor(colors.comment)
+            setPadding(p, p, p, p) 
+        })
 
-        // DIV
-        val divider = Divider(ctx).apply { setPadding(p, p, p, p) }
+        // Catbox Settings Section
+        mainLayout.addView(createSectionHeader("Catbox Settings"))
+        mainLayout.addView(createDescriptionText("Enter your catbox.moe userhash (leave empty for anonymous uploads)"))
 
-        // CATBOX SETTINGS HEADER
-        val catboxHeader = TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply { text = "Catbox Settings" }
-
-        // USERHASH INPUT BOX
-        val userhashInput = TextInput(ctx, "Userhash")
-        userhashInput.apply {
+        // Userhash Input
+        val userhashInput = TextInput(ctx, "Userhash").apply {
             editText.setText(settings.getString("catboxUserhash", ""))
-            editText.addTextChangedListener(object : TextWatcher() {
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-                override fun afterTextChanged(s: Editable) {}
-            })
+            editText.setTextColor(colors.foreground)
+            editText.setHintTextColor(colors.comment)
         }
+        mainLayout.addView(userhashInput)
 
-        // SAVE USERHASH BUTTON
+        // Save Userhash Button
         val userhashButton = Button(ctx).apply {
             text = "Save"
+            setTextColor(colors.background)
+            setBackgroundColor(colors.cyan)
             setOnClickListener {
                 settings.setString("catboxUserhash", userhashInput.editText.text.toString())
                 Utils.showToast("Saved")
             }
         }
+        mainLayout.addView(userhashButton)
 
-        // 2nd DIV
-        val secondDivider = Divider(ctx).apply { setPadding(p, p, p, p) }
+        // Divider
+        mainLayout.addView(Divider(ctx).apply { 
+            setBackgroundColor(colors.comment)
+            setPadding(p, p, p, p) 
+        })
 
-        // ADVANCED SETTINGS HEADER
-        val advHeader = TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply { text = "Advanced Settings" }
+        // Advanced Settings Section
+        mainLayout.addView(createSectionHeader("Advanced Settings"))
 
-        // CHECKED SETTINGS
+        // Checked Settings
         val uploadAllAttachments = Utils.createCheckedSetting(
-                ctx, CheckedSetting.ViewType.CHECK,
-                "Upload all attachment types", "Try to upload all attachment types instead of just images.\n(Warning: Might error)"
+            ctx, CheckedSetting.ViewType.CHECK,
+            "Upload all attachment types", 
+            "Try to upload all attachment types instead of just images.\n(Warning: Might error)"
         ).apply {
             isChecked = settings.getBool("uploadAllAttachments", false)
             setOnCheckedListener {
                 settings.setBool("uploadAllAttachments", it)
             }
         }
+        mainLayout.addView(uploadAllAttachments)
+
         val switchOffPlugin = Utils.createCheckedSetting(
-                ctx, CheckedSetting.ViewType.CHECK,
-                "Disable UITH", "Disable this plugin to send attachments normally.\nSlash command available: \"/uith disable\""
+            ctx, CheckedSetting.ViewType.CHECK,
+            "Disable UITH", 
+            "Disable this plugin to send attachments normally.\nSlash command available: \"/uith disable\""
         ).apply {
             isChecked = settings.getBool("pluginOff", false)
             setOnCheckedListener {
                 settings.setBool("pluginOff", it)
             }
         }
+        mainLayout.addView(switchOffPlugin)
 
-        // 3rd DIV
-        val thirdDivider = Divider(ctx).apply { setPadding(p, p, p, p) }
+        // Reset JSON Button
+        val resetButton = Button(ctx).apply {
+            text = "Reset JSON to Default"
+            setTextColor(colors.background)
+            setBackgroundColor(colors.red)
+            setOnClickListener {
+                settings.remove("jsonConfig")
+                Utils.showToast("JSON configuration reset to default")
+            }
+        }
+        mainLayout.addView(resetButton)
 
-        // LINKS HEADER
-        val linksHeader = TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply { text = "Links" }
+        // Divider
+        mainLayout.addView(Divider(ctx).apply { 
+            setBackgroundColor(colors.comment)
+            setPadding(p, p, p, p) 
+        })
 
-        // HELP/INFO
+        // Links Section
+        mainLayout.addView(createSectionHeader("Links"))
+
+        // Help/Info
         val helpInfo = TextView(ctx).apply {
             linksClickable = true
             text = "- UITH README: https://git.io/JSyri"
-            setTextColor(ColorCompat.getThemedColor(ctx, R.b.colorOnPrimary))
+            setTextColor(colors.orange)
         }
         Linkify.addLinks(helpInfo, Linkify.WEB_URLS)
+        mainLayout.addView(helpInfo)
 
-        addView(input)
-        addView(button)
-        addView(errorField)
-
-        addView(divider)
-
-        addView(catboxHeader)
-        
-        // CATBOX USERHASH - Moved here to appear directly under the header
-        TextView(ctx).apply {
-            text = "Enter your catbox.moe userhash (leave empty for anonymous uploads)"
-            setTextColor(ColorCompat.getThemedColor(ctx, R.b.colorOnPrimary))
-            setPadding(p, p, p, p)
-            addView(this)
-        }
-        
-        addView(userhashInput)
-        addView(userhashButton)
-
-        addView(secondDivider)
-
-        addView(advHeader)
-        addView(uploadAllAttachments)
-        addView(switchOffPlugin)
-        addView(resetButton)
-        addView(thirdDivider)
-        addView(linksHeader)
-        addView(helpInfo)
+        // Add main layout to the view
+        addView(mainLayout)
     }
 }
